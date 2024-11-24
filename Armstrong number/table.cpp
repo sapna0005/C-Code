1cf7d65adcf6d64c2c8fac3280c46dde992717e0
sapna0005 <sapna.sahani0005@gmail.com>
2024-11-24 23:29:31 +0530

    added table of 2

---
+++ Armstrong number/table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    for (i = 2; i<=5; ++i)
+    {
+    for(int k=1;k<=10; ++k)
+    {
+        cout<<i*k<<" ";
+    }
+    
+    
+    cout<<endl;
+    }
+}
