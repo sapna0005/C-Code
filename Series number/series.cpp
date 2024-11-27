129c85543304f9356667a1547f5429e973b96446
sapna0005 <sapna.sahani0005@gmail.com>
2024-11-27 11:54:12 +0530

    added series number

---
+++ Series number/series.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int term=0;
+    int sum=0;
+    for (int i= 0; i<=5; ++i)
+    {
+        term=term*10+1;
+        cout<<term<<" ";
+        
+    }
+    cout<<endl;
+}
