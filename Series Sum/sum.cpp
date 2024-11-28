6a9fb10d85162b552ad39a659d9a082c942bb154
sapna0005 <sapna.sahani0005@gmail.com>
2024-11-28 23:53:16 +0530

    added sum of series number

---
+++ Series Sum/sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int term=0;
+    int sum=0;
+    for (int i= 0; i<=5; ++i)
+    {
+        term=term*10+1;
+        sum=sum+term;
+        
+    }
+    cout<<sum<<" ";
+    
+}
