c6b56dd4e856bec70d0aa31e471cf154939f888a
sapna0005 <sapna.sahani0005@gmail.com>
2024-12-02 23:56:52 +0530

    added AP series

---
+++ Arithmetic Progression/Arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n=5;
+    int d=2;
+    int temp;
+    int a=1;
+    for (int i = 1; i<=n; ++i)
+    {
+        temp=a+(i*d);
+        cout<<temp;
+    }
+    
+}
